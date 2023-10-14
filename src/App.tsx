8d0a5369c41acf61ba9e984121c31587b7e7ae20
Przemek <kwasniewski.pj@gmail.com>
2023-10-14 17:52:32 +0200

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import GuessGame  from './guessTheWord/GuessGame'
 
 
 function App() {
-  const [word, setWord] = useState(0)
-
   return (
     <div>
       <GuessGame />
